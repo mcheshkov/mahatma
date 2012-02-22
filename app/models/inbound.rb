7bab7004ed6f7e96006c1e9d4c6be119960c7564
Mikhail Cheshkov <mcheshkov@gmail.com>
2012-02-23 00:42:37 +0700

    Fix in/outbound ip reference, fix forgotten @, remove index.html

--- app/models/inbound.rb
+++ app/models/inbound.rb
@@ -1,6 +1,5 @@
 class Inbound < ActiveRecord::Base
   set_table_name "inbound"
   validates_presence_of :ip, :ts, :bytes
-  belongs_to :ip
-  
+  belongs_to :ip, :foreign_key => :ip  
 end
--- app/models/outbound.rb
+++ app/models/outbound.rb
@@ -1,5 +1,5 @@
 class Outbound < ActiveRecord::Base
   set_table_name "outbound"
   validates_presence_of :ip, :ts, :bytes
-  belongs_to :ip
+  belongs_to :ip, :foreign_key => :ip
 end
