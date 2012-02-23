d173a15bb34bb9378977d380ed736507f0980b30
Mikhail Cheshkov <mcheshkov@gmail.com>
2012-02-23 15:29:04 +0700

    Fix field name in validation

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,5 @@
 class Department < ActiveRecord::Base
-  validates_presence_of :name, :user_id
+  validates_presence_of :name, :person_id
   validates_uniqueness_of :name
   validates_associated :person, :parent
   belongs_to :parent, :class_name => "Department"
