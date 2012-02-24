0cc21b01632f491d47834924e3360b5bd49c7b01
Mikhail Cheshkov <mcheshkov@gmail.com>
2012-02-24 16:12:37 +0700

    Add ip_as_String to ip model Fix ip form - blank user

--- app/helpers/ips_helper.rb
+++ app/helpers/ips_helper.rb
@@ -1,21 +1,2 @@
 module IpsHelper
-  def ip2str(ip)
-    o4=ip % (1 << 8)
-    ip>>=8
-    o3=ip % (1 << 8)
-    ip>>=8
-    o2=ip % (1 << 8)
-    ip>>=8
-    o1=ip % (1 << 8)
-    
-    o1.to_s + '.' + o2.to_s + '.' + o3.to_s + '.' + o4.to_s
-  end
-  def str2ip(str)
-    ip = 0
-    str.split('.').each{ |s|
-        ip <<= 8
-        ip += s.to_i
-    }
-    ip
-  end
 end
--- app/models/ip.rb
+++ app/models/ip.rb
@@ -7,4 +7,35 @@ class Ip < ActiveRecord::Base
   belongs_to :department
   has_many :inbounds, :foreign_key => "ip"
   has_many :outbounds, :foreign_key => "ip"
+
+  def ip_as_string
+    ip2str(ip)
+  end
+
+  def ip_as_string=(str)
+    self.ip = str2ip(str) if new_record?
+  end
+
+  private
+  def ip2str(ip)
+    return if !ip
+    o4=ip % (1 << 8)
+    ip>>=8
+    o3=ip % (1 << 8)
+    ip>>=8
+    o2=ip % (1 << 8)
+    ip>>=8
+    o1=ip % (1 << 8)
+    
+    o1.to_s + '.' + o2.to_s + '.' + o3.to_s + '.' + o4.to_s
+  end
+  def str2ip(str)
+    return if !str
+    ip = 0
+    str.split('.').each{ |s|
+        ip <<= 8
+        ip += s.to_i
+    }
+    ip
+  end
 end
