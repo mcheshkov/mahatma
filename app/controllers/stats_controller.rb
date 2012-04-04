43dae17293b04456c460088dfd22c385a7e28531
Mikhail Cheshkov <mcheshkov@gmail.com>
2012-04-04 16:33:10 +0700

    Move traffic counting logic to department model Add daily/nightly traffic

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,33 +7,6 @@ class StatsController < ApplicationController
     @from_date = Date.parse(params[:from_date]) if params[:from_date]
     @to_date = Date.parse(params[:to_date]) if params[:to_date]
 
-    @inbounds_by_id = Ip.joins(:inbounds).where("ts >= ? AND ts < ?",@from_date,@to_date).group(:department_id).sum(:bytes)
-    @outbounds_by_id = Ip.joins(:outbounds).where("ts >= ? AND ts < ?",@from_date,@to_date).group(:department_id).sum(:bytes)
-
     @departments = Department.all
-
-    collect_total_bytes
-  end
-
-  private
-
-  def collect_total_bytes
-    @total_inbounds_by_id = {}
-    @total_outbounds_by_id = {}
-
-    @departments.each do |d|
-      cur = d
-      in_bytes = @inbounds_by_id[d.id] || 0
-      out_bytes = @outbounds_by_id[d.id] || 0
-      while cur
-        @total_inbounds_by_id[cur.id] = 0 if !@total_inbounds_by_id[cur.id]
-        @total_inbounds_by_id[cur.id] += in_bytes
-
-        @total_outbounds_by_id[cur.id] = 0 if !@total_outbounds_by_id[cur.id]
-        @total_outbounds_by_id[cur.id] += out_bytes
-
-        cur = cur.parent
-      end
-    end
   end
 end
--- app/models/department.rb
+++ app/models/department.rb
@@ -6,4 +6,80 @@ class Department < ActiveRecord::Base
   has_many :children, :class_name => "Department", :foreign_key => "parent_id"
   belongs_to :person
   has_many :ips
+  has_many :inbounds, :through => :ips
+
+  def in_traffic(from_date,to_date)
+    inbounds.where("ts >= ? AND ts < ?",from_date,to_date).sum(:bytes)
+  end
+
+  def daily_in_traffic(from_date,to_date)
+    inbounds.where("ts >= ? AND ts < ?",from_date,to_date).where("(HOUR(ts) >= 8 AND HOUR(ts) < 20)").sum(:bytes)
+  end
+
+  def night_in_traffic(from_date,to_date)
+    inbounds.where("ts >= ? AND ts < ?",from_date,to_date).where("NOT (HOUR(ts) >= 8 AND HOUR(ts) < 20)").sum(:bytes)
+  end
+
+  def total_in_traffic(from_date,to_date)
+    res = in_traffic(from_date,to_date)
+    children.each do |c|
+      res += c.in_traffic(from_date,to_date)
+    end
+    res
+  end
+
+  def total_daily_in_traffic(from_date,to_date)
+    res = daily_in_traffic(from_date,to_date)
+    children.each do |c|
+      res += c.daily_in_traffic(from_date,to_date)
+    end
+    res
+  end
+
+  def total_night_in_traffic(from_date,to_date)
+    res = night_in_traffic(from_date,to_date)
+    children.each do |c|
+      res += c.night_in_traffic(from_date,to_date)
+    end
+    res
+  end
+
+
+
+  def out_traffic(from_date,to_date)
+    outbounds.where("ts >= ? AND ts < ?",from_date,to_date).sum(:bytes)
+  end
+
+  def daily_out_traffic(from_date,to_date)
+    outbounds.where("ts >= ? AND ts < ?",from_date,to_date).where("(HOUR(ts) >= 8 AND HOUR(ts) < 20)").sum(:bytes)
+  end
+
+  def night_out_traffic(from_date,to_date)
+    outbounds.where("ts >= ? AND ts < ?",from_date,to_date).where("NOT (HOUR(ts) >= 8 AND HOUR(ts) < 20)").sum(:bytes)
+  end
+
+  def total_out_traffic(from_date,to_date)
+    res = out_traffic(from_date,to_date)
+    children.each do |c|
+      res += c.out_traffic(from_date,to_date)
+    end
+    res
+  end
+
+  def total_daily_out_traffic(from_date,to_date)
+    res = daily_out_traffic(from_date,to_date)
+    children.each do |c|
+      res += c.daily_out_traffic(from_date,to_date)
+    end
+    res
+  end
+
+  def total_night_out_traffic(from_date,to_date)
+    res = night_out_traffic(from_date,to_date)
+    children.each do |c|
+      res += c.night_out_traffic(from_date,to_date)
+    end
+    res
+  end
+
 end
