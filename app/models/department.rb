71552219080261a74f66cfc601822251fc47abd9
Mikhail Cheshkov <mcheshkov@gmail.com>
2012-04-18 01:43:28 +0700

    Clean code - now single function traffic(dir,type,from_date,to_date)

--- app/models/department.rb
+++ app/models/department.rb
@@ -9,93 +9,33 @@ class Department < ActiveRecord::Base
   has_many :inbounds, :through => :ips
   has_many :outbounds, :through => :ips
 
-  @@all_in_traffic = {}
-  @@all_out_traffic = {}
-
-  def in_traffic(from_date,to_date)
-    daily_in_traffic(from_date,to_date) + night_in_traffic(from_date,to_date)
+  def traffic(dir,type,from_date,to_date)
+    @@all_traffic ||= get_all_traffic(from_date,to_date)
+    @@all_traffic[ [id,dir,type] ] || 0
   end
 
-  def daily_in_traffic(from_date,to_date)
-    @@all_in_traffic[ [from_date,to_date] ] ||= get_all_in_traffic(from_date,to_date)
-    @@all_in_traffic[ [from_date,to_date] ][ [id,1] ] || 0
-  end
-
-  def night_in_traffic(from_date,to_date)
-    @@all_in_traffic[ [from_date,to_date] ] ||= get_all_in_traffic(from_date,to_date)
-    @@all_in_traffic[ [from_date,to_date] ][ [id,0] ] || 0
-  end
-
-  def total_in_traffic(from_date,to_date)
-    res = in_traffic(from_date,to_date)
+  def total_traffic(dir,type,from_date,to_date)
+    res = traffic(dir,type,from_date,to_date)
     children.each do |c|
-      res += c.in_traffic(from_date,to_date)
-    end
-    res
-  end
-
-  def total_daily_in_traffic(from_date,to_date)
-    res = daily_in_traffic(from_date,to_date)
-    children.each do |c|
-      res += c.daily_in_traffic(from_date,to_date)
-    end
-    res
-  end
-
-  def total_night_in_traffic(from_date,to_date)
-    res = night_in_traffic(from_date,to_date)
-    children.each do |c|
-      res += c.night_in_traffic(from_date,to_date)
-    end
-    res
-  end
-
-
-
-  def out_traffic(from_date,to_date)
-    daily_out_traffic(from_date,to_date) + night_out_traffic(from_date,to_date)
-  end
-
-  def daily_out_traffic(from_date,to_date)
-    @@all_out_traffic[ [from_date,to_date] ] ||= get_all_out_traffic(from_date,to_date)
-    @@all_out_traffic[ [from_date,to_date] ][ [id,1] ] || 0
-  end
-
-  def night_out_traffic(from_date,to_date)
-    @@all_out_traffic[ [from_date,to_date] ] ||= get_all_out_traffic(from_date,to_date)
-    @@all_out_traffic[ [from_date,to_date] ][ [id,0] ] || 0
-  end
-
-  def total_out_traffic(from_date,to_date)
-    res = out_traffic(from_date,to_date)
-    children.each do |c|
-      res += c.out_traffic(from_date,to_date)
-    end
-    res
-  end
-
-  def total_daily_out_traffic(from_date,to_date)
-    res = daily_out_traffic(from_date,to_date)
-    children.each do |c|
-      res += c.daily_out_traffic(from_date,to_date)
-    end
-    res
-  end
-
-  def total_night_out_traffic(from_date,to_date)
-    res = night_out_traffic(from_date,to_date)
-    children.each do |c|
-      res += c.night_out_traffic(from_date,to_date)
+      res += c.traffic(dir,type,from_date,to_date)
     end
     res
   end
 
   private
-  def get_all_in_traffic(from_date,to_date)
-    Ip.joins(:inbounds).where("ts >= ? AND ts < ?",from_date,to_date).group(:department_id,"(HOUR(ts)>=8 AND HOUR(ts)<20)").sum(:bytes)
-  end
+  def get_all_traffic(from_date,to_date)
+    res = {}
 
-  def get_all_out_traffic(from_date,to_date)
-    Ip.joins(:outbounds).where("ts >= ? AND ts < ?",from_date,to_date).group(:department_id,"(HOUR(ts)>=8 AND HOUR(ts)<20)").sum(:bytes)
+    t = Ip.joins(:inbounds).where("ts >= ? AND ts < ?",from_date,to_date).group(:department_id,"(HOUR(ts)>=8 AND HOUR(ts)<20)").sum(:bytes)
+    t.each do |k,v|
+      res[ [k[0], :in, k[1]==1 ? :day : :night] ] = v
+    end
+
+    t = Ip.joins(:outbounds).where("ts >= ? AND ts < ?",from_date,to_date).group(:department_id,"(HOUR(ts)>=8 AND HOUR(ts)<20)").sum(:bytes)
+    t.each do |k,v|
+      res[ [k[0], :out, k[1]==1 ? :day : :night] ] = v
+    end
+
+    res
   end
 end
